994b4676bf2e514f5fa4d9fb463095852aa43d9c
windsorroyalapps <137676262+windsorroyalapps@users.noreply.github.com>
2026-06-04 23:29:56 +1000

    Debug: Fix gradle plugin versions and library aliases in version catalog"

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application") version "8.5.1" apply false
     id("com.android.library") version "8.5.1" apply false
     kotlin("android") version "1.9.20" apply false
-    kotlin("jvm") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
