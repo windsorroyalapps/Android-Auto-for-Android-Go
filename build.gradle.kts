2f217cc11df55f35dc78dd9a8019bec7bbb7c763
windsorroyalapps <137676262+windsorroyalapps@users.noreply.github.com>
2026-06-04 23:25:24 +1000

    Fix: Add proper root build.gradle.kts and gradle configuration files

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.5.1" apply false
+    id("com.android.library") version "8.5.1" apply false
+    kotlin("android") version "1.9.20" apply false
+}
+
+group = "com.aago"
+version = "1.0.0"
